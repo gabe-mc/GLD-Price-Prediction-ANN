7df3bda2c22dc5d2821aad15ae27bd832c75e4e1
Gabriel McFadyen <gabrielmcfadyen@gmail.com>
2024-10-24 14:02:43 -0400

    Refactored to create better encapencapsulation, created new models folder for trained models to be saved to.

--- ann_predictor.py
+++ ann_predictor.py
@@ -1,102 +1,98 @@
-"""Predict the price of GLD using an artifical neural network"""
+"""Class creating and training the ANN model from time series data"""
 
-import math
 import pandas as pd
 import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as D
 
+from data_transformation import min_max_scaler, split
+from tensor import Tensor
 
-# Splitting into testing and training data
+# Import data
 
 data = pd.read_csv("data/training_data.csv")
 data = data.drop("Date", axis=1)
 
-data = data.sample(frac=1)
+# Split and normalize data
 
-num_rows = data.shape[0]
-train_split_percentage = 0.75
-split = math.floor(num_rows*train_split_percentage)
-
-testing_data = data[0:split]
-training_data = data[split:]
-
-
-# Preprocessing data
-
-def min_max_scaler(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Normalizes the array data into values between 0 and 1. Takes a panada DataFrame and creates a new panda DataFrame.
-    """
-    result = pd.DataFrame(columns=data.columns)
-    for column in data:
-        loop_index = 0
-        for value in data[column].values:
-            loop_index += 1
-            column_max = data[column].max()
-            column_min = data[column].min()
-            new_value = (float(value) - float(column_min)) / \
-                (float(column_max) - float(column_min))
-            result.at[loop_index, column] = new_value
-
-    return result
-
-
-target_scaled = np.array(min_max_scaler(testing_data))
-features_scaled = np.array(min_max_scaler(training_data))
+training_data, testing_data = split(data, 0.75)
+training_data = min_max_scaler(training_data)
+testing_data = min_max_scaler(testing_data)
 
 testing_target = testing_data["GLD Price"]
 testing_features = testing_data.drop("GLD Price", axis=1)
 
 training_target = training_data["GLD Price"]
-training_features = training_data.drop("GLD Price", axis=1)
+training_features = training_data.drop("GLD Price", axis=1) 
+    
+# Convert DataFrames into Pytorch tensors, along with their respective Pytorch DataLoaders
 
+batch_size = 64
 
-# Creating ANN
+train_tensor = Tensor(features=training_features, target=training_target)
+train_dataloader = D.DataLoader(dataset=train_tensor, batch_size=batch_size, shuffle=False)
 
-learning_rate = 0.6
+test_tensor = Tensor(features=testing_features, target=testing_target)
+test_dataloader = D.DataLoader(dataset=test_tensor, batch_size=batch_size, shuffle=False)
 
+# Creating our prediction ANN
 
 class GLDPredictor(nn.Module):
     """
-    Artificial Neural Network predicting the price of $GLD.
-    - Three layers of neruons
-    - 
+    Artificial Neural Network predicting the price of $GLD. 
+    Three layers of neruons, using affine linear transformations and ReLU activation functions.
     """
 
     def __init__(self):
         super(GLDPredictor, self).__init__()
-        self.layer1 = nn.Linear(9, 12)
-        self.layer2 = nn.Linear(12, 12)
-        self.output = nn.Linear(12, 1)
+        self.layer1 = nn.Linear(in_features=9, out_features=12, dtype=torch.float64)
+        self.layer2 = nn.Linear(in_features=12, out_features=12, dtype=torch.float64)
+        self.output = nn.Linear(in_features=12, out_features=1, dtype=torch.float64)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         x = F.relu(self.output(x))
-        return x
 
+        return x
+    
+# Training GLDPredictor on our training data
 
 model = GLDPredictor()
-loss_function = nn.L1Loss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Instantiate our loss function
+
+learning_rate = 0.1
+loss_function = nn.L1Loss() # Mean absoloute error
+optimizer = torch.optim.Adam(model.parameters(), learning_rate)
+
+# Our training loop
 
 epochs = 100
+loss_values = []
+
 for epoch in range(epochs):
-    # Initalize tensors
-    inputs = torch.tensor(training_features.to_numpy(), dtype=torch.float32)
-    labels = torch.tensor(training_target.to_numpy(), dtype=torch.float32)
+    for features, target in train_dataloader:
+        optimizer.zero_grad()
 
-    # Forward pass
-    outputs = model(inputs)
-    loss = loss_function(outputs, labels.unsqueeze(1))
+        # Forward pass
+        prediction = model(features)
+        loss = loss_function(prediction, target.unsqueeze(-1))
+        loss_values.append(loss.item())
 
-    # Backwards pass
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        # Backward pass + optimization
+        loss.backward()
+        optimizer.step()
 
-    if (epoch+1) % 10 == 0:
-        print(f'Epoch: {epoch+1}. Remaining: {epochs - epoch+1}. Loss: {loss.item():.4f}')
+        # Print out loss
+        if epoch % 10 == 0:
+            print(loss.item())
+
+print(f"Training Complete. Final loss: {loss_values[-1]}")
+
+# Save our trained model
+
+torch.save(model.state_dict(), "models/GLDtrained1.plt")
---
+++ tensor.py
@@ -0,0 +1,24 @@
+"""A single tensor class, creating and storing torch.tensor objects"""
+
+import torch
+import torch.utils.data as D
+
+class Tensor(D.Dataset):
+    """
+    Class representing torch.tensor data. Is able to take DataFrame objects and convert them into Pytorch tensors.
+    
+    Instance Variables:
+        features: takes a Panda DataFrame object of numerical features
+        target: takes a single Panda DataFrame column of numerical targets
+        len: length of tensor
+    """
+    def __init__(self, features, target):
+        self.features = torch.tensor(data=features.values, dtype=torch.float64)
+        self.target = torch.tensor(data=target.values, dtype=torch.float64)
+        self.len = self.features.shape[0]
+
+    def __getitem__(self, index):
+        return self.features[index], self.target[index]
+   
+    def __len__(self):
+        return self.len
